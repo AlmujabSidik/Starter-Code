86fa4a33e20006b4b235401efa7bfdcc23659d04
Almujab Sidik <almujabsidik@gmail.com>
2024-02-27 22:54:43 +0900

    Layouting with Blade

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="footer-left">
+    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauv.al/">Muhamad Nauval Azhar</a>. Code By Almujab Sidik
+</div>
+{{--<div class="footer-right">--}}
+{{--    2024--}}
+{{--</div>--}}
